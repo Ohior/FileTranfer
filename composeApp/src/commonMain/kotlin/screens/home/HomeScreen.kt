f5f66255be13e9a4590f8c42d43820076330ed73
Ohiorenua <50307290+Ohior@users.noreply.github.com>
2024-05-17 19:49:36 +0100

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
@@ -0,0 +1,43 @@
+package screens.home
+
+import Greeting
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.model.rememberScreenModel
+import cafe.adriel.voyager.core.screen.Screen
+import filetranfer.composeapp.generated.resources.Res
+import filetranfer.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+object HomeScreen:Screen {
+    @OptIn(ExperimentalResourceApi::class)
+    @Composable
+    override fun Content() {
+        val screenModel = rememberScreenModel { HomeScreenModel() }
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/screens/home/HomeScreenModel.kt
@@ -0,0 +1,6 @@
+package screens.home
+
+import cafe.adriel.voyager.core.model.ScreenModel
+
+class HomeScreenModel:ScreenModel {
+}
